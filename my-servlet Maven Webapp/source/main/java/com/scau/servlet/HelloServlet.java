6bc0d62018ae09f1aee5eaf4516cb3e694a5bd38
mou <weimou_peng@163.com>
2016-11-04 10:12:57 +0800

    commit my first maven project

---
+++ my-servlet Maven Webapp/source/main/java/com/scau/servlet/HelloServlet.java
@@ -0,0 +1,31 @@
+package com.scau.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8951614379332210608L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		resp.getWriter().println(this.getServletName());
+	}
+
+}
